fcf28818be73b7ae6648796502c011542fa9bbb9
Nurideen Abubakari <nurideen82@gmail.com>
2024-12-03 13:30:52 -0800

    End of section 8

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -16,34 +16,36 @@ public class AccountController(DataContext context, ITokenService tokenService) 
     public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
         if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
-        using var hmac = new HMACSHA512();
 
-        var user = new AppUser
-        {
-            UserName = registerDto.Username.ToLower(),
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-            PasswordSalt = hmac.Key
-        };
+         return Ok();
+         //using var hmac = new HMACSHA512();
 
-        context.Users.Add(user);
-        await context.SaveChangesAsync();
+         //var user = new AppUser
+        //{
+         //   UserName = registerDto.Username.ToLower(),
+         //   PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+         //   PasswordSalt = hmac.Key
+        //};
 
-        return new UserDto
-        {
-            Username = user.UserName,
-            Token = tokenService.CreateToken(user)
-        };
+        //context.Users.Add(user);
+        //await context.SaveChangesAsync();
+
+        //return new UserDto
+        //{
+         //   Username = user.UserName,
+         //   Token = tokenService.CreateToken(user)
+        //};
     }
 
     [HttpPost("login")]  //account/login
     public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
-        var user = await context.Users.FirstOrDefaultAsync(x => 
+       var user = await context.Users.FirstOrDefaultAsync(x => 
             x.UserName == loginDto.Username.ToLower());
 
         if (user == null) return Unauthorized("Invalid username");
 
-        using var hmac = new HMACSHA512(user.PasswordSalt);
+       using var hmac = new HMACSHA512(user.PasswordSalt);
 
         var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
 
